721ab11fbc2379d1b5f6a956e31c77bdaf0975e2
Mirshod <python3526@gmail.com>
2024-07-20 20:22:09 +0500

    minor improvement

--- smtplib.py
+++ smtplib.py
@@ -10,7 +10,6 @@
 # msg['Subject'] = 'Test mail'
 # msg['From'] = 'admin@example.com'
 # msg['To'] = 'info@example.com'
-
 # with smtplib.SMTP('localhost', port) as server:
 
 # server.sendmail(sender, receivers, msg.as_string())
