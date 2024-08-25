99858f9edfb6d24c4d677499fcdc8f444b8c0c42
Mirshod <python3526@gmail.com>
2024-08-25 22:06:26 +0500

    minor improvement

--- static/js/congrats_style.js
+++ static/js/congrats_style.js
@@ -1,10 +1,4 @@
-/* by rikanutyy */
-document.addEventListener('DOMContentLoaded', function () {
-    const card = document.getElementById('interactive-card');
-
-    // Add event listener for tap/click
-    card.addEventListener('click', function () {
-        // Toggle the 'hover' class on click/tap
-        card.classList.toggle('hover');
-    });
+document.querySelector('.toggle-btn').addEventListener('click', function() {
+    var card = document.querySelector('.card');
+    card.classList.toggle('effect');
 });
