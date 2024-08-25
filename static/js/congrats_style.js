26dd7ed6d34183fcc6076101215442a4562fa61b
Mirshod <python3526@gmail.com>
2024-08-25 21:46:59 +0500

    added a new page

--- static/js/congrats_style.js
+++ static/js/congrats_style.js
@@ -1,1 +1,10 @@
 /* by rikanutyy */
+document.addEventListener('DOMContentLoaded', function () {
+    const card = document.getElementById('interactive-card');
+
+    // Add event listener for tap/click
+    card.addEventListener('click', function () {
+        // Toggle the 'hover' class on click/tap
+        card.classList.toggle('hover');
+    });
+});
