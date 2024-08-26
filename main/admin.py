c8149d88c9bd26bfb827ca499e207a735de3d836
Mirshod <python3526@gmail.com>
2024-08-27 04:04:41 +0500

    added request log model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import RequestsLog
+
+
+@admin.register(RequestsLog)
+class RequestsLogAdmin(admin.ModelAdmin):
+    list_display = ['request_time', 'ip_address', 'device_type', 'referred_to', 'browser', 'os', 'is_mobile',
+                    'is_tablet', 'is_pc']
+    list_filter = ['request_time', 'ip_address']
+    search_fields = ['device_type', 'ip_address']
--- main/views.py
+++ main/views.py
@@ -1,46 +1,50 @@
 from datetime import datetime
 
-import requests
-from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from user_agents import parse
 
-from root import settings
+from main.models import RequestsLog
 
 
 def home_page(request):
     if request.method == 'GET':
+        send_sms(request, 'Home Page')
         return render(request, 'index.html')
 
 
 def choices_page(request):
     if request.method == 'GET':
-        send_sms(request)
+        send_sms(request, 'Choices Page')
         return render(request, 'button_index.html')
 
 
 def blooming_flower_page(request):
     if request.method == 'GET':
+        send_sms(request, 'Blooming Flower Page')
         return render(request, 'blooming.html')
 
 
 def calming_home_page(request):
     if request.method == 'GET':
+        send_sms(request, 'Calming Home Page')
         return render(request, 'calming_style.html')
 
 
 def solar_system_page(request):
     if request.method == 'GET':
+        send_sms(request, 'Solar System Page')
         return render(request, 'universe_index.html')
 
 
 def intro_page(request):
     if request.method == 'GET':
+        send_sms(request, 'Intro Page')
         return render(request, 'intro_style.html')
 
 
 def alerting(request):
     if request.method == 'GET':
+        send_sms(request, 'Alerting Page')
         request.session['popup_message'] = \
             "You currently can not enter this page. Sorry but it is not fully done yet, Orange !"
         popup_message = request.session.pop('popup_message', None)
@@ -53,54 +57,64 @@ def alerting(request):
 
 def firefly_page(request):
     if request.method == 'GET':
+        send_sms(request, 'Firefly Page')
         return render(request, 'firefly_style.html')
 
 
 def just_home_page(request):
     if request.method == 'GET':
+        send_sms(request, 'Just Home Page')
         return render(request, 'home_script.html')
 
 
 def tree_page(request):
     if request.method == 'GET':
+        send_sms(request, 'Tree Page')
         return render(request, 'tree_style.html')
 
 
 def congrats_page(request):
     if request.method == 'GET':
+        send_sms(request, 'Congrats Page')
         return render(request, 'congrats_style.html')
 
 
 def neon_rain_page(request):
     if request.method == 'GET':
+        send_sms(request, 'Neon Rain Page')
+        send_sms(request, 'Neon Rain Page')
         return render(request, 'neon_style.html')
 
 
 def street_rain_page(request):
     if request.method == 'GET':
+        send_sms(request, 'Street Rain Page')
         return render(request, 'rainy_style.html')
 
 
 def simple_rain_page(request):
     if request.method == 'GET':
+        send_sms(request, 'Simple Rain Page')
         return render(request, 'simple_style.html')
 
 
 def portal_page(request):
     if request.method == 'GET':
+        send_sms(request, 'Portal Page')
         return render(request, 'portal_style.html')
 
 
 def bunny_page(request):
     if request.method == 'GET':
+        send_sms(request, 'Bunny Page')
         return render(request, 'bunny_style.html')
 
 
-def send_sms(entered_request):
+def send_sms(entered_request, in_url):
     # Get the user's IP address
     x_forwarded_for = entered_request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        ip_address = x_forwarded_for.split(',')[0]  # Use the first IP in the list
+        ip_address = x_forwarded_for.split(',')[0]
     else:
         ip_address = entered_request.META.get('REMOTE_ADDR')
 
@@ -108,90 +122,15 @@ def send_sms(entered_request):
     user_agent_string = entered_request.META.get('HTTP_USER_AGENT', '')
     user_agent = parse(user_agent_string)
 
-    # Additional META information
-    referer = entered_request.META.get('HTTP_REFERER', 'No Referrer')
-    accept_language = entered_request.META.get('HTTP_ACCEPT_LANGUAGE', 'No Language Info')
-    host = entered_request.META.get('HTTP_HOST', 'No Host Info')
+    current_time = datetime.now()
 
-    # Get location data based on IP
-    try:
-        response = requests.get(f'https://ipapi.co/{ip_address}/json/')
-        location_data = response.json()
-        city = location_data.get('city')
-        country = location_data.get('country_name')
-    except requests.RequestException as e:
-        city = 'Unknown'
-        country = 'Unknown'
-
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-    # Create the email message
-    subject = "Request to Website"
-    message = (
-        f"IP Address: {ip_address}\n"
-        f"City: {city}\n"
-        f"Country: {country}\n\n"
-        f"User Agent: {user_agent_string}\n"
-        f"Browser: {user_agent.browser.family}\n"
-        f"OS: {user_agent.os.family}\n"
-        f"Device Type: {user_agent.device.family}\n"
-        f"Is Mobile: {user_agent.is_mobile}\n"
-        f"Is Tablet: {user_agent.is_tablet}\n"
-        f"Is PC: {user_agent.is_pc}\n\n"
-        f"Referrer: {referer}\n"
-        f"Accept Language: {accept_language}\n"
-        f"Host: {host}\n\n"
-        f"Request Time: {current_time}"
-    )
-
-    # Send the email
-    recipient_list = ['trading3526@gmail.com']
-    send_mail(subject,
-              message,
-              settings.DEFAULT_FROM_EMAIL,
-              recipient_list,
-              fail_silently=False)
-
-# class HomePage(View):
-#     def get(self, request):
-#         return render(request, 'index.html')
-
-
-# class ChoicesPage(View):
-#     def get(self, request):
-#         return render(request, 'button_index.html')
-
-
-# class BloomingFlowersPage(View):
-#     def get(self, request):
-#         return render(request, 'blooming.html')
-
-
-# class CalmingHomePage(View):
-#     def get(self, request):
-#         return render(request, 'calming_style.html')
-
-
-# class SolarSystemPage(View):
-#     def get(self, request):
-#         return render(request, 'universe_index.html')
-
-
-# class OrangePage(View):
-#     def get(self, request):
-#         return render(request, 'orange_style.html')
-
-
-# class FireFlyPage(View):
-#     def get(self, request):
-#         return render(request, 'firefly_style.html')
-
-
-# class JustHomePage(View):
-#     def get(self, request):
-#         return render(request, 'home_script.html')
-
-
-# class TreePage(View):
-#     def get(self, request):
-#         return render(request, 'tree_style.html')
+    RequestsLog.objects.create(
+        ip_address=ip_address,
+        browser=user_agent.browser.family,
+        os=user_agent.os.family,
+        device_type=user_agent.device.family,
+        is_mobile=user_agent.is_mobile,
+        is_tablet=user_agent.is_tablet,
+        is_pc=user_agent.is_pc,
+        referred_to=in_url,
+        request_time=current_time)
