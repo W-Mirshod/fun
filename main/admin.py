60535feafef2dff8d9566e86dd69720e319387ab
Mirshod <python3526@gmail.com>
2024-08-27 04:29:04 +0500

    added request log model

--- main/admin.py
+++ main/admin.py
@@ -5,7 +5,7 @@ from main.models import RequestsLog
 
 @admin.register(RequestsLog)
 class RequestsLogAdmin(admin.ModelAdmin):
-    list_display = ['request_time', 'ip_address', 'device_type', 'referred_to', 'browser', 'os', 'is_mobile',
+    list_display = ['ip_address', 'request_time', 'device_type', 'referred_to', 'browser', 'os', 'is_mobile',
                     'is_tablet', 'is_pc']
     list_filter = ['request_time', 'ip_address']
     search_fields = ['device_type', 'ip_address']
